b6dfd3a2a1a3f5057904872cc77beb81cabb9bb2
Ania <annazofka@gmail.com>
2023-07-23 14:20:38 -0700

    create functionality for adding new todos

---
+++ app.js
@@ -0,0 +1,23 @@
+const addForm = document.querySelector("#add-form");
+const list = document.querySelector("#todos");
+
+const generateTemplateForToDo = (toDoInput) => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${toDoInput}</span>
+    <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const toDoInput = addForm.addtodo.value.trim(); //trim white spaces
+
+  if (toDoInput.length) {
+    generateTemplateForToDo(toDoInput);
+    addForm.reset();
+  }
+});
